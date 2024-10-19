834fb0b7736d23914a994ea2a074eae0656abb83
geludwig <74719181+geludwig@users.noreply.github.com>
2024-10-19 12:41:20 +0200

    Add files via upload

--- tmcm16xx.cpp
+++ tmcm16xx.cpp
@@ -12,6 +12,12 @@ cmd[6] = (value >> 8) & 0xFF;
 cmd[7] = value & 0xFF;
 */
 
+// ################################################################################
+// #                                                                              #
+// #                              PRIVATE FUNCTIONS                               #
+// #                                                                              #
+// ################################################################################
+
 /*  Calculate Value And Checksum :
     int value */
 void TMCM16XX::calcValueChecksum(int value) {
@@ -38,27 +44,6 @@ TMCM16XX::TMCM16XX() {
     for (int i = 0; i < 9; i++) {
         cmd[i] = 0;
     }
-    cmd[0] = 0;     // 1 byte: Module address
-    cmd[3] = 0;     // 1 byte: Motor or Bank number
-}
-
-/*  Get Firmware version.
-    arg:    int type
-    ret:    char[9]
-*/
-const unsigned char* TMCM16XX::getFirmwareVersion(int type) {
-    cmd[1] = 136;
-    cmd[2] = type;
-    calcValueChecksum(0);
-    return cmd;
-}
-
-/*  Get last command.
-    arg:    na
-    ret:    char[9]
-*/
-const unsigned char* TMCM16XX::getLastCommand() const {
-    return cmd;
 }
 
 /*  Rotate Right.
@@ -66,8 +51,7 @@ const unsigned char* TMCM16XX::getLastCommand() const {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::setRotateRight(int value) {
-    cmd[1] = 1;
-    cmd[2] = 0;
+    cmd[1] = ROR;
     calcValueChecksum(value);
     return cmd;
 }
@@ -77,30 +61,7 @@ const unsigned char* TMCM16XX::setRotateRight(int value) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::setRotateLeft(int value) {
-    cmd[1] = 2;
-    cmd[2] = 0;
-    calcValueChecksum(value);
-    return cmd;
-}
-
-/*  Move Absolute.
-    arg:    int position
-    ret:    char[9]
-*/
-const unsigned char* TMCM16XX::setMoveAbs(int value) {
-    cmd[1] = 4;
-    cmd[2] = 0;
-    calcValueChecksum(value);
-    return cmd;
-}
-
-/*  Move Relative.
-    arg:    int offset
-    ret:    char[9]
-*/
-const unsigned char* TMCM16XX::setMoveRel(int value) {
-    cmd[1] = 4;
-    cmd[2] = 1;
+    cmd[1] = ROL;
     calcValueChecksum(value);
     return cmd;
 }
@@ -110,9 +71,30 @@ const unsigned char* TMCM16XX::setMoveRel(int value) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::setStop() {
-    cmd[1] = 3;
-    cmd[2] = 0;
+    cmd[1] = MST;
     calcValueChecksum(0);
+    return cmd;
+}
+
+/*  Move Absolute.
+    arg:    int position
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::setMoveAbs(int value) {
+    cmd[1] = MVP;
+    cmd[1] = ABS;
+    calcValueChecksum(value);
+    return cmd;
+}
+
+/*  Move Relative.
+    arg:    int offset
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::setMoveRel(int value) {
+    cmd[1] = MVP;
+    cmd[2] = REL;
+    calcValueChecksum(value);
     return cmd;
 }
 
@@ -127,7 +109,7 @@ const unsigned char* TMCM16XX::setStop() {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::setAxisParameter(int type, int value) {
-    cmd[1] = 5;
+    cmd[1] = SAP;
     cmd[2] = type;
     calcValueChecksum(value);
     return cmd;
@@ -138,7 +120,7 @@ const unsigned char* TMCM16XX::setAxisParameter(int type, int value) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::getAxisParameter(int type) {
-    cmd[1] = 6;
+    cmd[1] = GAP;
     cmd[2] = type;
     calcValueChecksum(0);
     return cmd;
@@ -149,7 +131,7 @@ const unsigned char* TMCM16XX::getAxisParameter(int type) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::saveAxisParameter(int type) {
-    cmd[1] = 7;
+    cmd[1] = STAP;
     cmd[2] = type;
     calcValueChecksum(0);
     return cmd;
@@ -160,7 +142,7 @@ const unsigned char* TMCM16XX::saveAxisParameter(int type) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::loadAxisParameter(int type) {
-    cmd[1] = 8;
+    cmd[1] = RSAP;
     cmd[2] = type;
     calcValueChecksum(0);
     return cmd;
@@ -171,7 +153,7 @@ const unsigned char* TMCM16XX::loadAxisParameter(int type) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::setGlobalParameter(int type, int value) {
-    cmd[1] = 9;
+    cmd[1] = SGP;
     cmd[2] = type;
     calcValueChecksum(value);
     return cmd;
@@ -182,7 +164,7 @@ const unsigned char* TMCM16XX::setGlobalParameter(int type, int value) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::getGlobalParameter(int type) {
-    cmd[1] = 10;
+    cmd[1] = GGP;
     cmd[2] = type;
     calcValueChecksum(0);
     return cmd;
@@ -193,7 +175,7 @@ const unsigned char* TMCM16XX::getGlobalParameter(int type) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::saveGlobalParameter(int type) {
-    cmd[1] = 11;
+    cmd[1] = STGP;
     cmd[2] = type;
     calcValueChecksum(0);
     return cmd;
@@ -204,7 +186,7 @@ const unsigned char* TMCM16XX::saveGlobalParameter(int type) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::loadGlobalParameter(int type) {
-    cmd[1] = 12;
+    cmd[1] = RSGP;
     cmd[2] = type;
     calcValueChecksum(0);
     return cmd;
@@ -221,6 +203,69 @@ const unsigned char* TMCM16XX::loadGlobalParameter(int type) {
     ret:    char[9]
 */
 const unsigned char* TMCM16XX::setCurrentMax(int value) {
-    setAxisParameter(6, value);
+    setAxisParameter(MAXCUR, value);
+    return cmd;
+}
+
+/*  Get Actual Current.
+    arg:    int value
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::getCurrent(int value) {
+    setAxisParameter(ACTCUR, value);
+    return cmd;
+}
+
+/*  Set Target Current.
+    arg:    int value
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::setCurrent(int value) {
+    setAxisParameter(TARCUR, value);
+    return cmd;
+}
+
+/*  Set PID Current Delay.
+    arg:    int value
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::setCurrentPidDelay(int value) {
+    setAxisParameter(PIDCURRENTDELAY, value);
+    return cmd;
+}
+
+/*  Set PID Current P Parameter.
+    arg:    int value
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::setCurrentP(int value) {
+    setAxisParameter(PCUR, value);
+    return cmd;
+}
+
+/*  Set PID Current I Parameter.
+    arg:    int value
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::setCurrentI(int value) {
+    setAxisParameter(ICUR, value);
+    return cmd;
+}
+
+/*  Get PID Error.
+    arg:    int value
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::getCurrentPidError(int value) {
+    setAxisParameter(PIDCURERR, value);
+    return cmd;
+}
+
+/*  Get PID Error Sum.
+    arg:    int value
+    ret:    char[9]
+*/
+const unsigned char* TMCM16XX::getCurrentPidErrorSum(int value) {
+    setAxisParameter(PIDCURERRSUM, value);
     return cmd;
 }
--- tmcm16xx.h
+++ tmcm16xx.h
@@ -6,16 +6,115 @@ class TMCM16XX {
         unsigned char cmd[9];
         void calcValueChecksum(int value);
 
+        /* Command list */
+        enum command {
+            ROR = 1,
+            ROL,
+            MST,
+            MVP,
+            SAP,
+            GAP,
+            STAP,
+            RSAP,
+            SGP,
+            GGP,
+            STGP,
+            RSGP,
+            SIO = 14,
+            GIO,
+            CALC = 19,
+            COMP,
+            JC,
+            JA,
+            CSUB,
+            RSUB,
+            WAIT = 27,
+            STOP,
+            CALCX = 33,
+            AAP,
+            AGP
+        };
+
+        /* Type list for MVP command */
+        enum mvp {
+            ABS,
+            REL
+        };
+
+        /* Type list for SAP, GAP, STAP, RSAP, AAP commands */
+        enum parameter {
+            TARPOS,
+            ACTPOS,
+            TARSPEED,
+            ACTSPEED,
+            MAXRAMPVEL,
+            MAXCUR = 6,
+            TARREACHVEL,
+            MOTORHALTVEL = 9,
+            TARREACHDIST,
+            ACCVEL,
+            RAMPSPEED = 13,
+            THERMWINDTIME = 25,
+            I2TLIM,
+            I2TSUM,
+            I2TEXCEEDCOUNT,
+            I2TCLEARFLAG,
+            RUNMIN,
+            BLDCRST,
+            PIDDELAY = 133,
+            PIDCURRENTDELAY,
+            ENVELRAMP = 146,
+            ACTCUR = 150,
+            SUPVOLTAGE,
+            TEMP,
+            TARCUR = 155,
+            ERRFLAGS,
+            COMMUTATION = 159,
+            ENCSETNULL = 161,
+            SWSETNULL,
+            ENCCLEAR,
+            STOPSW,
+            ENCOFFSET,
+            STOPSWPOL,
+            PCUR = 172,
+            ICUR,
+            STARTCUR = 177,
+            PIDCURERR = 200,
+            PIDCURERRSUM,
+            HALLANGLE = 210,
+            ENCANGLE,
+            CONTROLANGLE,
+            PIDPOSERR = 226,
+            PIDVELERR = 228,
+            PIDVELERRSUM,
+            PPOS,
+            PVEL = 234,
+            IVEL,
+            SINEINITSPEED = 241,
+            SINEINITDELAY = 244,
+            OVERVOLTAGEPROT,
+            INITSINEMODE = 249,
+            ENCSTEPS,
+            ENCDIR,
+            MOTORPOLES = 253,
+            HALLINV,
+            ENDRIVER
+        };
+
     public:
         TMCM16XX();
+        void debug();
 
-        const unsigned char* getFirmwareVersion(int type);
-        const unsigned char* getLastCommand() const;
+        /* HIGH LEVEL FUNCTIONS */
+
         const unsigned char* setRotateRight(int velocity);
         const unsigned char* setRotateLeft(int velocity);
         const unsigned char* setMoveAbs(int position);
         const unsigned char* setMoveRel(int offset);
         const unsigned char* setStop();
+
+        /* PARAMETER FUNCTIONS */
+
         const unsigned char* setAxisParameter(int type, int value);
         const unsigned char* getAxisParameter(int type);
         const unsigned char* saveAxisParameter(int type);
@@ -25,6 +124,8 @@ class TMCM16XX {
         const unsigned char* saveGlobalParameter(int type);
         const unsigned char* loadGlobalParameter(int type);
 
+        /* CURRENT FUNCTIONS */
+
         const unsigned char* setCurrentMax(int value);
         const unsigned char* setCurrent(int value);
         const unsigned char* getCurrent(int value);
@@ -32,6 +133,7 @@ class TMCM16XX {
         const unsigned char* setCurrentP(int value);
         const unsigned char* setCurrentI(int value);
         const unsigned char* getCurrentPidError(int value);
+        const unsigned char* getCurrentPidErrorSum(int value);
 
 
 };
