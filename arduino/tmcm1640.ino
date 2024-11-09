91eac4555d807baa454aa1b2df37a33cff899fe4
geludwig <74719181+geludwig@users.noreply.github.com>
2024-11-09 15:28:00 +0100

    Update tmcm1640.ino

--- arduino/tmcm1640.ino
+++ arduino/tmcm1640.ino
@@ -2,21 +2,26 @@
 
 TMCM16XX tmcm;  // define tmcm
 
-const uint8_t rtsPin = 2; // hardware flow control pin (RTS)
-const char preDelay = 1;  // define the RTS delay in ms before sending data (should be > 0)
-const char postDelay = 0; // define the RTS delay in ms after sending data (should be 0)
+const uint8_t ledPin = 13;
 
-const unsigned char* sendMsg;   // define send array
-const int* receiveMsg;          // define receive array
-const char maxWaitReceive = 20; // max wait time for receive message in ms
+const uint8_t rtsPin = 2;     // hardware flow control pin (RTS)
+const uint8_t preDelay = 1;   // define the RTS delay in ms before sending data (should be 5 > predelay >= 0)
+const uint8_t postDelay = 0;  // define the RTS delay in ms after sending data (should be 0)
+
+const unsigned char* sendMsg;       // define send array
+const int* receiveMsg;              // define receive array
+const uint8_t maxWaitReceive = 10;  // max wait time for receive message in ms
 
 // >> SETUP
 void setup() {
-  pinMode(rtsPin, OUTPUT);  // define rtsPin as output
+  pinMode(ledPin, OUTPUT);
+  pinMode(rtsPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(rtsPin, LOW);
 
-  Serial.begin(9600);   // start serial console
-  Serial1.begin(9600);  // start serial RS485
-  delay(200);
+  Serial.begin(115200);   // start serial console
+  Serial1.begin(115200);  // start serial RS485 115200
+  delay(1000);
 
   sendMsg = (const unsigned char*)malloc(9); // allocate memory for send array
   receiveMsg = (const int*)malloc(5);        // allocate memory for receive array
@@ -24,14 +29,31 @@ void setup() {
   TMCM16XX(); // init tmcm
   tmcm.setSerialReceiveId(1);  // set serial receive device id (probably usb=0, rs485=1)
 
-  sendMsg = tmcm.setMoveRotateRight(100); // init sendMsg with command (here rotate right with speed 100)
-  setMessage(sendMsg);                    // send message to serial
-  receiveMsg = getMessage();              // receive and decode message
-  printMessage(receiveMsg);               // print message
+  sendMsg = tmcm.setStop();
+  setMessage(sendMsg);        // send message to serial
+  receiveMsg = getMessage();  // receive and decode message
+  printMessage(receiveMsg);   // print message
 }
 
 // >> LOOP
 void loop() {
+  digitalWrite(ledPin, HIGH);
+  sendMsg = tmcm.setMoveRotateRight(300);
+  setMessage(sendMsg);
+  receiveMsg = getMessage();
+  printMessage(receiveMsg);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+
+  digitalWrite(ledPin, HIGH);
+  sendMsg = tmcm.setMoveRotateLeft(300);
+  setMessage(sendMsg);
+  receiveMsg = getMessage();
+  printMessage(receiveMsg);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
 
 }
 
@@ -48,26 +70,24 @@ void setMessage(const unsigned char* send){
 // >> RECEIVE RS485 MESSAGE
 const int* getMessage(){
   char tempBuf[9];
-
-  for (char i=0; i<maxWaitReceive; i++){  // wait loop
-    if (Serial1.available() == 9){        // check for 9 bytes in buffer
-      for (char j=0; j<9; j++){           // copy serial to buffer
+  for (uint8_t i=0; i<maxWaitReceive; i++){   // wait loop
+    if (Serial1.available() == 9){            // check for 9 bytes in buffer
+      for (uint8_t j=0; j<9; j++){            // copy serial to buffer
         tempBuf[j] = Serial1.read();
       }
-      return tmcm.decodeReceive(tempBuf); // return and decode
+      return tmcm.decodeReceive(tempBuf);  // return and decode
     }
     else{
       delay(1);
     }
   }
-  return tmcm.decodeReceive(tempBuf);     // return empty buffer in case of error
+  return tmcm.decodeReceive(tempBuf); // return empty buffer in case of error
 }
 
 // >> PRINT MESSAGE (for array definition look under "const int* TMCM16XX::decodeReceive()" function)
 void printMessage(const int* rec){
-  for (char i=0; i<5; i++){
-    Serial.print(rec[i]);
-    Serial.print(" ");
+  for (uint8_t i=0; i<5; i++){
+    Serial.println(rec[i], HEX);
   }
   Serial.println();
 }
